f8dbded3419c468d933cb279348234f34c3f18e9
Aleksandra Vereshchagina <alexvereschagina@gmail.com>
2023-04-23 23:13:44 +0200

    task26

---
+++ task26/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+int res = Sum(number);
+Console.WriteLine(res);
+
+int Sum (int a)
+{
+    int result = 0;
+    while (number > 0)
+    {
+        result += number % 10;
+        number = number / 10;
+    }
+    return result;
+}
